6ad6f35191a6a5f77dc035c543d7c77dfdaaaa01
mfunyu <mfunyu@student.42tokyo.jp>
2022-09-28 00:01:13 +0900

    update: logo add Link

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,12 +1,12 @@
-import { Box, Text } from '@chakra-ui/react'
+import { LinkBox, LinkOverlay } from '@chakra-ui/react'
 
 const Logo = () => {
   return (
-    <Box px={8}>
-      <Text fontSize="lg" fontWeight="bold">
+    <LinkBox px={8}>
+      <LinkOverlay href="/" fontSize="lg" fontWeight="bold">
         mfunyu / U
-      </Text>
-    </Box>
+      </LinkOverlay>
+    </LinkBox>
   )
 }
 
